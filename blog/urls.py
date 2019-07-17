bb0091bf57cc49651fd0143d55d314b672c04e65
reed501 <reeds501@gmail.com>
2019-07-16 20:59:25 -0700

    Fixed urls and blog description

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:bid>/', views.posts, name='post'),
-    path('<int:bid>/<int:pid>/', views.comments, name='comment')
+    path('<int:bid>/', views.PostView.as_view(), name='post'),
+    path('<int:pid>/comments', views.comments, name='comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -9,10 +9,22 @@ class IndexView(generic.ListView):
     model = Blog
     template_name = 'blog/index.html'
 
-def posts(request, bid):
-    posts = Post.objects.filter(blogpost__blog_id=bid)
-    return render(request, 'blog/blog.html', {'posts': posts})
+#def posts(request, bid):
+ #   posts = Post.objects.filter(blogpost__blog_id=bid)
+  #  return render(request, 'blog/blog.html', {'posts': posts})
 
-def comments(request, bid, pid):
+class PostView(generic.ListView):
+    context_object_name = 'posts'
+    template_name = 'blog/blog.html'
+    
+    def get_queryset(self):
+        return Post.objects.filter(blogpost__blog_id=self.kwargs['bid'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blog'] = Blog.objects.get(id=self.kwargs['bid'])
+        return context
+
+def comments(request, pid):
     comms = Comment.objects.filter(postcomment__post_id=pid)
     return render(request, 'blog/comment.html', {'comments': comms})
