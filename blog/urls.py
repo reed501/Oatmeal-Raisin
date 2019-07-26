4494ba89b92a36fa5668685d2f0207932d6e5c2e
Alvin Heng <alvin.hadiheng@gmail.com>
2019-07-26 16:57:59 -0700

    Fixed signup: both passwords has to be the same. Login works with UI. Implemented UI for both login and signup

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    # path('', views.IndexView.as_view(), name='index'),
     path('<int:bid>/', views.PostView.as_view(), name='post'),
     path('<int:pid>/comments', views.CommentView.as_view(), name='comment'),
     path('<int:bid>/addpost', views.make_post, name='makepost'),
@@ -12,5 +12,5 @@ urlpatterns = [
     path('<int:pid>/addLike', views.addLike, name='addLike'),
     path('<int:pid>/addDislike', views.addDislike, name='addDislike'),
     path('signup', views.make_account, name='makeaccount'),
-    path('login', views.log_in, name='signin')
+    path('', views.log_in, name='signin')
 ]
